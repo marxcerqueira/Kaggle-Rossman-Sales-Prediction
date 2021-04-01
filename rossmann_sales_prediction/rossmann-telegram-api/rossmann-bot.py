8a1a4eca8dd67265a38099f4fe13635b4b4d1814
Marx Cerqueira <marxv49@gmail.com>
2021-03-31 23:51:33 -0300

    initial commit

---
+++ rossmann_sales_prediction/rossmann-telegram-api/rossmann-bot.py
@@ -0,0 +1,148 @@
+import os
+import pandas as pd
+import json
+import requests
+import telegram
+import matplotlib.pyplot as plt
+import seaborn           as sns
+from flask import Flask, request, Response 
+from io import BytesIO
+
+# constants
+TOKEN = '1772058988:AAEy1JExctBfpdXfA9E6AtfP_VG_zmSuvyQ'
+
+# # info about the Bot
+# https://api.telegram.org/bot1772058988:AAEy1JExctBfpdXfA9E6AtfP_VG_zmSuvyQ/getMe
+
+# # get updates
+# https://api.telegram.org/bot1772058988:AAEy1JExctBfpdXfA9E6AtfP_VG_zmSuvyQ/getUpdates
+
+# # Webhook
+# https://api.telegram.org/bot1772058988:AAEy1JExctBfpdXfA9E6AtfP_VG_zmSuvyQ/setWebhook?url=https://431c9427f9233d.localhost.run/
+
+# # Webhook Heroku
+# https://api.telegram.org/bot1772058988:AAEy1JExctBfpdXfA9E6AtfP_VG_zmSuvyQ/setWebhook?url=https://git.heroku.com/rossman-bot-telegram.git/
+
+
+# # send message
+# https://api.telegram.org/bot1772058988:AAEy1JExctBfpdXfA9E6AtfP_VG_zmSuvyQ/sendMessage?chat_id=669387603&text=Hi Marx, I am doing good, tks!
+# 669387603
+
+def send_message(chat_id, text):
+    url = 'https://api.telegram.org/bot{}/'.format( TOKEN )
+    url = url + 'sendMessage?chat_id={}'.format(chat_id)
+    
+    r = requests.post(url, json = {'text': text})
+    print('Status Code{}'.format(r.status_code))
+
+    return None
+
+def load_dataset(store_id):
+    # loading test dataset
+    df10 = pd.read_csv('test.csv', low_memory = False)
+    df_store_raw = pd.read_csv('store.csv', low_memory = False)
+
+    # merge test dataset + store attributes dataset
+    df_test = pd.merge(df10, df_store_raw, how = 'left', on = 'Store')
+
+    # choose store for prediction
+    df_test = df_test[df_test['Store'] == store_id]
+
+    if not df_test.empty:
+        # remove closed days
+        df_test = df_test[df_test['Open'] != 0]
+        df_test = df_test[~df_test['Open'].isnull()]
+        df_test = df_test.drop('Id', axis = 1)
+
+        # convert dataframe to json
+        data = json.dumps(df_test.to_dict(orient = 'records'))
+    else:
+        data = 'error'
+        
+    return data
+
+def predict(data):
+    # API Call
+    url = 'https://rossmann-model-pred.herokuapp.com/rossmann/predict'
+    header = {'Content-type': 'application/json' } #indica para a api qual tipo de dados que estamos recebendo
+    data = data
+
+    r = requests.post(url, data = data, headers = header)
+    print('Status Code {}'.format(r.status_code))
+
+    d1 = pd.DataFrame(r.json(), columns = r.json()[0].keys())
+
+    return d1
+
+def parse_message(message):
+    chat_id = message['message']['chat']['id']
+    store_id = message['message']['text']
+
+    store_id = store_id.replace('/', '') # remove the / that telegram uses
+
+    try:
+        store_id = int(store_id)
+    
+    except ValueError:
+        store_id = 'error'
+
+    return chat_id, store_id
+
+# API Initialize
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST']) # create the endpoint/route (from where the user message will come)
+
+def index(): # runs every time the endpoint / is called
+
+    if request.method == 'POST':
+        message = request.get_json() # get the json data
+        chat_id, store_id = parse_message(message)
+
+        if store_id != 'error':
+            #loading data
+            data = load_dataset(store_id)
+
+            if data != 'error':
+                #prediction
+                d1 = predict(data)
+
+                #calculation
+                d2 = d1[['store', 'prediction']].groupby('store').sum(0).reset_index()
+                
+                #send message
+                intro = 'Generating sales forecast for store {}...'.format(d2['store'].values[0])
+                send_message(chat_id, intro)
+
+                msg = 'Store Number {} will sell R$ {:,.2f} in the next 6 weeks'.format(
+                            d2['store'].values[0], 
+                            d2['prediction'].values[0])
+
+                # sending graphics with de prediction
+                # fig = plt.figure()
+                # sns.lineplot(x = 'week_of_year', y = 'prediction', data = d1)
+                # plt.title('Weekly Sales FOrecast: Store {}'.format(d2['store'].values[0]))
+                # plt.xlabel('Week Year (starting from week 31 - July 19th)')
+                # plt.ylabel('Sales Prediction')
+
+                # buffer = BytesIO()
+                # fig.savefig(buffer, formart = 'png')
+                # buffer.seek(0)
+                # Bot.send_photo(chat_id=chat_id, photo = buffer)
+
+                send_message(chat_id, msg)
+                return Response('Ok', status = 200)
+
+            else:
+                send_message(chat_id, 'Store Not Available for prediction')
+                return Response('Ok', status = 200) 
+
+        else:
+            send_message(chat_id, 'Wrong Store ID. Please use only integer store numbers')
+            return Response('Ok', status = 200)
+    else:
+        return '<h1> Rossmann Telegram BOT </h1>'
+
+if __name__ == '__main__':
+    port = os.environ.get('PORT', 5000)
+    app.run(host = '0.0.0.0', port = port, debug = True)
---
+++ rossmann_sales_prediction/webapp/rossmann/Rossmann.py
@@ -0,0 +1,166 @@
+import pickle
+import pandas as pd
+import numpy as np
+import math
+import datetime
+import inflection
+
+class Rossmann(object): # a classe rossman recebe um objeto. Iremos colocar todas limpezas, transformações, encondings
+    def __init__(self): 
+        self.home_path                     = ''
+        self.competition_distante_scaler   = pickle.load(open(self.home_path + 'parameters/competition_distance_scaler.pkl', 'rb'))
+        self.competition_time_month_scaler = pickle.load(open(self.home_path + 'parameters/competition_time_month_scaler.pkl', 'rb'))
+        self.promo_time_week_scaler        = pickle.load(open(self.home_path + 'parameters/promo_time_week_scaler.pkl', 'rb'))
+        self.year_scaler                   = pickle.load(open(self.home_path + 'parameters/year_scaler.pkl', 'rb'))    
+        self.store_type_scaler             = pickle.load(open(self.home_path + 'parameters/store_type_scaler.pkl', 'rb'))
+         
+    def data_cleaning(self, df1):
+
+        ### Rename Columns
+        cols_old = ['Store', 'DayOfWeek', 'Date', 'Open', 'Promo', 'StateHoliday', 'SchoolHoliday', 
+                    'StoreType', 'Assortment', 'CompetitionDistance', 'CompetitionOpenSinceMonth',
+                    'CompetitionOpenSinceYear', 'Promo2', 'Promo2SinceWeek', 
+                    'Promo2SinceYear', 'PromoInterval']
+
+        snakecase = lambda x: inflection.underscore(x)
+
+        cols_new = list( map( snakecase, cols_old ) )
+
+        #Rename Columns
+        df1.columns = cols_new
+
+        #Change datatype of column data to datetime
+        df1['date'] = pd.to_datetime( df1[ 'date' ] )
+        
+        # Fillout NA values
+        # competition_distance         
+        df1['competition_distance'] = df1['competition_distance'].apply(lambda x: 200000.0 if math.isnan( x ) else x )
+
+        # competition_distance
+        df1['competition_distance'] = df1['competition_distance'].apply(lambda x: 200000.0 if math.isnan( x ) else x )
+
+        # competition_open_since_month
+        df1['competition_open_since_month'] = df1.apply( lambda x: x['date'].month if math.isnan( x['competition_open_since_month'] ) else x['competition_open_since_month'],
+                                                    axis = 1 )
+        # competition_open_since_year
+        df1['competition_open_since_year'] = df1.apply( lambda x: x['date'].year if math.isnan( x['competition_open_since_year'] ) else x['competition_open_since_year'],
+                                                    axis = 1 )
+        # promo2_since_week
+        df1['promo2_since_week'] = df1.apply( lambda x: x['date'].week if math.isnan( x['promo2_since_week'] ) else x['promo2_since_week'],
+                                                    axis = 1 )
+        # promo2_since_year
+        df1['promo2_since_year'] = df1.apply( lambda x: x['date'].year if math.isnan( x['promo2_since_year'] ) else x['promo2_since_year'],
+                                                    axis = 1 )
+        # promo_interval
+        month_map = {1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5:'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sep', 10: 'Oct', 11: 'Nov', 12: 'Dec'}
+
+        df1['promo_interval'].fillna( 0, inplace = True)
+        df1['month_map'] = df1['date'].dt.month.map( month_map )
+        df1['is_promo'] = df1[[ 'promo_interval', 'month_map' ]].apply(lambda x: 0 if x['promo_interval'] == 0 else 1 if x['month_map'] in x['promo_interval'].split(',') else 0, axis = 1)
+
+        ### Change Types
+        df1 = df1.astype({'competition_open_since_month':'int64', 'competition_open_since_year':'int64', 'promo2_since_week':'int64', 'promo2_since_year':'int64' })
+        
+        return df1
+
+    def feature_engineering(self, df2):
+        # year
+        df2['year'] = df2['date'].dt.year
+
+        # month
+        df2['month'] = df2['date'].dt.month
+
+        # day
+        df2['day'] = df2['date'].dt.day
+
+        # week of year
+        df2['week_of_year'] = df2['date'].dt.isocalendar().week.astype('int64')
+
+        # year week
+        df2['year_week'] = df2['date'].dt.strftime( '%Y-%W' )
+
+        # competition since
+        df2['competition_since'] = df2.apply(lambda x: datetime.datetime( year = x['competition_open_since_year'], month = x['competition_open_since_month'], day=1 ), axis = 1)
+        df2['competition_time_month'] = ( ( df2['date'] - df2['competition_since'] )/30 ).apply( lambda x: x.days ).astype( 'int64' )
+
+        # promo since
+        df2['promo_since'] = df2['promo2_since_year'].astype( str ) + '-' + df2['promo2_since_week'].astype( str )
+        df2['promo_since'] = df2['promo_since'].apply( lambda x: datetime.datetime.strptime( x + '-1', '%Y-%W-%w' ) - datetime.timedelta( days=7 ) )
+        df2['promo_time_week'] = ( ( df2['date'] - df2['promo_since'] )/7 ).apply( lambda x: x.days ).astype( 'int64' )
+
+        # assortment
+        df2['assortment'] = df2['assortment'].apply( lambda x: 'basic' if x == 'a' else 'extra' if x == 'b' else 'extended' )
+
+        # state holiday
+        df2['state_holiday'] = df2['state_holiday'].apply( lambda x: 'public_holiday' if x == 'a' else 'easter_holiday' if x == 'b' else 'christmas' if x == 'c' else 'regular_day' )
+
+        ### Line Filtering
+        df2 = df2[df2['open'] != 0]
+
+        ### Columns Selection
+        cols_drop = ['open', 'promo_interval', 'month_map']
+        df2 = df2.drop( cols_drop, axis=1 )
+        
+        return df2
+
+    def data_preparation(self, df5):
+        # Rescaliing
+        # competition distance
+        df5['competition_distance'] = self.competition_distante_scaler.fit_transform( df5[['competition_distance']].values ) 
+        
+        # competition time month
+        df5['competition_time_month'] = self.competition_time_month_scaler.fit_transform( df5[['competition_time_month']].values )
+        
+        # promo time week
+        df5['promo_time_week'] = self.promo_time_week_scaler.fit_transform( df5[['promo_time_week']].values )
+
+        # year
+        df5['year'] = self.year_scaler.fit_transform( df5[['year']].values )
+
+        ### Feature Transformation
+        #### Encoding
+
+        # state_holiday (it's a state, holidays are moments) - One Hot Enconding
+        df5 = pd.get_dummies(df5, prefix = ['state_holiday'], columns = ['state_holiday'], dtype  = 'int64' )
+
+        # store_type - Label Encoder
+        df5['store_type'] = self.store_type_scaler.fit_transform( df5['store_type'] )
+        
+        # assortment - Ordinal Encoder
+        assortment_dict = {'basic': 1, 'extra': 2, 'extended': 3}
+        df5['assortment'] = df5['assortment'].map( assortment_dict )
+
+        #### Nature Transformation
+        # day of week
+        df5['day_of_week_sin'] = df5['day_of_week'].apply( lambda x: np.sin( x * ( 2. * np.pi/7 ) ) )
+        df5['day_of_week_cos'] = df5['day_of_week'].apply( lambda x: np.cos( x * ( 2. * np.pi/7 ) ) )
+
+        # month
+        df5['month_sin'] = df5['month'].apply( lambda x: np.sin( x * ( 2. * np.pi/12 ) ) )
+        df5['month_cos'] = df5['month'].apply( lambda x: np.cos( x * ( 2. * np.pi/12 ) ) )
+
+        # day 
+        df5['day_sin'] = df5['day'].apply( lambda x: np.sin( x * ( 2. * np.pi/30 ) ) )
+        df5['day_cos'] = df5['day'].apply( lambda x: np.cos( x * ( 2. * np.pi/30 ) ) )
+
+        # week of year
+        df5['week_of_year_sin'] = df5['week_of_year'].apply( lambda x: np.sin( x * ( 2. * np.pi/52 ) ) )
+        df5['week_of_year_cos'] = df5['week_of_year'].apply( lambda x: np.cos( x * ( 2. * np.pi/52 ) ) )
+        
+        # Converting dtypes after transformation
+        print(df5.columns)
+        df5 = df5.astype({'store_type': 'int64' })
+        # df5 = df5.astype({'store_type': 'int64' ,'state_holiday_christmas':'int64', 'state_holiday_easter_holiday':'int64', 'state_holiday_public_holiday':'int64', 'state_holiday_regular_day':'int64' })
+
+        cols_selected = ['store', 'promo', 'store_type', 'assortment', 'competition_distance', 'competition_open_since_month', 'competition_open_since_year', 'promo2', 'promo2_since_week', 'promo2_since_year', 'competition_time_month', 'promo_time_week', 'day_of_week_sin', 'day_of_week_cos','month_cos', 'month_sin', 'day_sin', 'day_cos', 'week_of_year_cos', 'week_of_year_sin']
+
+        return df5[cols_selected]
+    
+    def get_prediction(self, model, original_data, test_data):
+        # prediction
+        pred = model.predict(test_data)
+        
+        # join pred into the original data so people can undestandt it the new table with prediction column
+        original_data['prediction'] = np.expm1(pred)
+        
+        return original_data.to_json(orient = 'records', date_format = 'iso')
